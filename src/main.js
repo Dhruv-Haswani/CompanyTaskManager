af88e5248bd5fb55378b1b04ddb355f7603c9ed9
Dhruv Haswani <2024.dhruv.haswani@ves.ac.in>
2026-08-27 22:36:31 +0530

    Finalize Experiment 1 structure and clean up root files

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './style.css'
-
-document.querySelector('#app').innerHTML = `
-  <main class="flex min-h-screen items-center justify-center bg-blue-600">
-    <h1 class="text-4xl font-bold text-white">
-      Company Task Manager
-    </h1>
-  </main>
-`
