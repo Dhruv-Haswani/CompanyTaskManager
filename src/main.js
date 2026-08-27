291ca22797037e7fe4e7f173ee2601e059e8f31a
Dhruv Haswani <2024.dhruv.haswani@ves.ac.in>
2026-08-27 20:19:49 +0530

    Initial Company Task Manager setup

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = `
+  <main class="flex min-h-screen items-center justify-center bg-blue-600">
+    <h1 class="text-4xl font-bold text-white">
+      Company Task Manager
+    </h1>
+  </main>
+`
