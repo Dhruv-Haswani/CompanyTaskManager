0e913e9202777bdf2cb4800ff4a34516a97e7cce
Dhruv-Haswani <2024.dhruv.haswani@ves.ac.in>
2026-09-21 23:22:05 +0530

    Add files via upload

---
+++ Task.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, 'Task title is required'],
+      trim: true,
+    },
+    category: {
+      type: String,
+      default: 'General',
+      trim: true,
+    },
+    status: {
+      type: String,
+      enum: ['Pending', 'Completed'],
+      default: 'Pending',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Task', TaskSchema);
